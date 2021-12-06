ea2f46a9df2b21b58e67bfe9b0528c47211ff4c8
Razvan Ciobanu <razvan.ciobanu@lummetry.ai>
2021-12-06 13:56:47 +0200

    changed repickle files

--- xperimental/repickle_data.py
+++ xperimental/repickle_data.py
@@ -3,10 +3,10 @@ import pickle as pk
 
 if __name__ == '__main__':
   path_in = [
-    './_cache/_data/20211203_154548_x_data',
-    './_cache/_data/20211203_154548_y_data',
+    # './_cache/_data/20211203_154548_x_data',
+    # './_cache/_data/20211203_154548_y_data',
     './_cache/_data/dict_lbl',
-    './_cache/_data/embgen_full_embeds'
+    # './_cache/_data/embgen_full_embeds'
     './_cache/_data/lai_ro_i2w_191K',
     './_cache/_data/lai_embeddings_191K'
     './_cache/_data/20211203_154548_labels_dict'
