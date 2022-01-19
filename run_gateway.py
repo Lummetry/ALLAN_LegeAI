edcdd8f17b520e6a36bfbbc67784cec91dfa8aff
LegeAI <94859277+LegeAI@users.noreply.github.com>
2022-01-19 13:55:53 +0200

    Added get_conf endpoint

--- run_gateway.py
+++ run_gateway.py
@@ -64,7 +64,7 @@ if __name__ == '__main__':
 
   gtw = FlaskGateway(
     log=log,
-    # server_names=['get_tags', 'get_qa', 'get_sim', 'get_aprox'],
+    # server_names=['get_tags', 'get_qa', 'get_sim', 'get_aprox', 'get_conf'],
     workers_location='endpoints',
     workers_suffix='Worker',
     host=host,
