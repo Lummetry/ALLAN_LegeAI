87c8cfbb965f7a7b11e1f7e4376ae6c48ad8c482
filipmanisor <filip.manisor@gmail.com>
2022-01-27 13:37:56 +0200

    get_conf: improved Name recognition with less strict search

--- endpoints/get_conf.py
+++ endpoints/get_conf.py
@@ -3,10 +3,11 @@
 from libraries.model_server_v2 import FlaskWorker
 
 import constants as ct
-import numpy as np
 import spacy
 import re
 import phonenumbers
+from string import punctuation
+from utils.utils import simple_levenshtein_distance
 
 
 _CONFIG = {
@@ -28,6 +29,7 @@ MATCHED_NERS = {'PERSON', 'LOC'}
 PERSON_UPPERCASE = 1
 PERSON_PROPN = 2
 PERSON_TWO_WORDS = 3
+MAX_LEV_DIST = 3
 
 # ADDRESS
 MIN_LOC_LENGTH = 10
@@ -38,8 +40,8 @@ ADDRESS_MIN_TOKENS = 3
 EMAIL_REG = re.compile(r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b')
 
 # TELEPHONE
-REG_START = r'(?:(?<!\d\w)|(?<=^))'
-REG_END = r'(?:(?=$)|(?!\d\w))'
+REG_START = r'(?:(?<!\d|\w)|(?<=^))'
+REG_END = r'(?:(?=$)|(?!\d|\w))'
 PHONE_REGS = {
     # 0722215678
     r'\d{4}[ .-]?\d{3}[ .-]?\d{3}',
@@ -145,18 +147,32 @@ class GetConfWorker(FlaskWorker):
     def next_name_code(self, code):
         """ Get the next code for a new name. """
         
-        last_letter = code[-1]
         
-        if last_letter == 'Z':
+        if code[0] == 'Z':
             next_code = 'A' * (len(code) + 1)
         else:
-            next_code = code[:-1] + chr(ord(last_letter) + 1)
+            next_code = [chr(ord(c) + 1) for c in code]
+            next_code = ''.join(next_code)
             
         return next_code
     
+    def find_name(self, name, person_dict):
+        """ Find a name in the dictionary of names. """
+        
+        low_name = name.lower()
+        
+        for (n, code) in person_dict.items():
+            lev_dist = simple_levenshtein_distance(n.lower(), low_name, 
+                                                   normalize=False)
+            if n.lower() == low_name or lev_dist < MAX_LEV_DIST:
+                return code
+            
+        return None
+            
+    
     def match_ner(self, nlp, text,
                   ners=MATCHED_NERS,
-                  person_checks=[]
+                  person_checks=[],
                   address_checks=[]
                  ):
         """ Return the position of spaCy named entities in a text. """
@@ -166,9 +182,10 @@ class GetConfWorker(FlaskWorker):
             person_checks = [person_checks]
         
         doc = nlp(text)
+        print(doc.ents)
         
         person_dict = {}
-        person_code = 'A'
+        current_code = 'A'
         
         for ent in doc.ents:
             
@@ -198,7 +215,12 @@ class GetConfWorker(FlaskWorker):
                             if token.pos_ != 'PROPN':
                                 end_char = token.idx + len(token.text)
                                 break
-                    
+                            
+                # Ignore leading and trailing punctuation
+                while text[start_char] in punctuation:
+                    start_char += 1
+                while text[end_char - 1] in punctuation:
+                    end_char -= 1
                 
                 if is_match:
                     matches[start_char] = [start_char, end_char, "NUME"]
@@ -206,12 +228,15 @@ class GetConfWorker(FlaskWorker):
                     person = text[start_char:end_char]
                     if self.debug:
                         print(person)
-                        
-                    if person_dict and not person in person_dict:
-                        # Add the name to the dictionary
-                        person_dict[person] = person_code
+                                           
+                    person_code = self.find_name(person, person_dict)
+                    if not person_code:
                         # Get the next code for names
-                        person_code = self.next_name_code(person_code)
+                        person_code = current_code
+                        current_code = self.next_name_code(current_code)
+                        
+                    # Add the name to the dictionary
+                    person_dict[person] = person_code
                         
                 
             elif ent.label_ == 'LOC':
@@ -267,7 +292,7 @@ class GetConfWorker(FlaskWorker):
                         
                 if valid_match:        
                     start, end = self.find_match(match, text, res)
-                    res[start] = [start, end, 'PHONE']
+                    res[start] = [start, end, 'TELEFON']
                     if self.debug:
                         print(match)
                 
@@ -276,7 +301,7 @@ class GetConfWorker(FlaskWorker):
     
             for match in matches:
                 start, end = self.find_match(match, text, res)
-                res[start] = [start, end, 'PHONE']
+                res[start] = [start, end, 'TELEFON']
                 if self.debug:
                     print(match)
             
@@ -314,30 +339,41 @@ class GetConfWorker(FlaskWorker):
         matches.update(self.match_email(doc))
         
         # Match address and name
-        matches, person_dict = self.match_ner(self.nlp_model, doc, person_checks=[PERSON_PROPN, PERSON_TWO_WORDS])
-        matches.update(matches)  
+        ner_matches, person_dict = self.match_ner(self.nlp_model, doc, person_checks=[PERSON_PROPN, PERSON_TWO_WORDS])
+        matches.update(ner_matches)  
         print(person_dict)
 
         # Match phone
         matches.update(self.match_phone(doc, check_strength=PHONE_REG_VALID))
               
-        return doc, matches
+        return doc, matches, person_dict
 
     def _post_process(self, pred):
         
-        doc, matches = pred
+        doc, matches, person_dict = pred
+        print(matches)
         
+        # Order matches 
         match_tuples = list(matches.values())
         match_starts = list(sorted(matches.keys(), reverse=True))
-        
+    
+        # Replace all confidential information (except names) in text
         hidden_doc = doc
         for key in match_starts:
             [start, end, label] = matches[key]
-            hidden_doc = hidden_doc[:start] + '***' + hidden_doc[end:]
+            if label != 'NUME':
+                hidden_doc = hidden_doc[:start] + '***' + hidden_doc[end:]
+        
+        # Replace names with their codes
+        for (name, code) in person_dict.items():
+            hidden_doc = hidden_doc.replace(name, code)
         
         res = {}
         res['positions'] = match_tuples
         res['output'] = hidden_doc
+        
+        # if self.debug:
+        #     print(hidden_doc)
         
         return res
 
@@ -352,14 +388,35 @@ if __name__ == '__main__':
       'DEBUG' : True,
       'PRINT_RESULT': True,
       
-      # 'DOCUMENT': """Un contribuabil al cărui cod numeric personal este 2548016600768 va completa caseta "Cod fiscal" astfel:""",
+           # 'DOCUMENT': """Un contribuabil al cărui cod numeric personal este 1520518054675 va completa caseta "Cod fiscal" astfel:""",
       
-      'DOCUMENT': """Se desemnează domnul Cocea Radu, avocat, cocea@gmail.com, 0216667896 domiciliat în municipiul Bucureşti, Bd. Laminorului nr. 84, sectorul 1, legitimat cu C.I. seria RD nr. 040958, eliberată la data de 16 septembrie 1998 de Secţia 5 Poliţie Bucureşti, CNP 1561119034963, în calitate de administrator special.""", 
+      # 'DOCUMENT': """Se desemnează domnul Cocea Radu, avocat, cocea@gmail.com, 0216667896 domiciliat în municipiul Bucureşti, Bd. Laminorului nr. 84, sectorul 1, legitimat cu C.I. seria RD nr. 040958, eliberată la data de 16 septembrie 1998 de Secţia 5 Poliţie Bucureşti, CNP 1561119034963, în calitate de administrator special. Se desemneaza si doamna Alice Munteanu cu telefon 0216654343, domiciliata in Bd. Timisoara nr. 107 """, 
         
       # 'DOCUMENT': """Cod numeric personal: 1505952103022. Doi copii minori înregistraţi în documentul de identitate.""",
         
       # 'DOCUMENT': """Bătrîn Cantemhir-Marian, porcine, Str. Cardos Iacob nr. 34, Arad, judeţul Arad, 1850810020101. 
       # Almăjanu Steliana, porcine, Comuna Peretu, judeţul Teleorman, 2580925341708.""",
+      
+      'DOCUMENT' : """
+      S-au luat în examinare apelurile declarate de Parchetul de pe lângă Înalta Curte de Casaţie şi Justiţie – Direcţia naţională Anticorupţie şi de inculpatul Popa Vasile Constantin împotriva sentinţei penale nr. 194/PI din 13 martie 2018 a Curţii de Apel Timişoara – Secţia Penală.
+Dezbaterile au fost consemnate în încheierea de şedinţă din data de 09 ianuarie 2020,  ce face parte integrantă din prezenta decizie şi având nevoie de timp pentru a delibera, în baza art. 391 din Codul de procedură penală a amânat pronunţarea pentru azi 22 ianuarie 2020, când în aceeaşi compunere a pronunţat următoarea decizie:
+ÎNALA CURTE
+	Asupra apelurilor penale de faţă;
+În baza lucrărilor din dosar, constată următoarele:
+Prin sentinţa penală nr. 194/PI din 13 martie 2018 a Curţii de Apel Timişoara – Secţia Penală, pronunţată în dosarul nr.490/35/2014, în baza art. 386 din Codul de procedură penală a respins cererea de schimbare a încadrării juridice a faptei de sustragere sau distrugere de înscrisuri, prev. de art. 242 al. 1 şi 3 din Codul penal, cu aplic. art. 5 din Codul penal, în cea de sustragere sau distrugere de probe ori de înscrisuri, prev. de art. 275 al. 1 şi 2 din Codul penal, formulată de inculpatul POPA VASILE CONSTANTIN.
+	În baza art. 386 din Codul de procedură penală a fost admisă cererea de schimbare a încadrării juridice a faptelor formulată de D.N.A. – Serviciul Teritorial Timişoara, sens în care:
+- s-a dispus schimbarea încadrării juridice a infracţiunii de luare de mită, prev. de art. 6 din Legea nr. 78/2000 rap. la art. 289 din Codul penal, rap. la art. 7 alin.1 lit. b din Legea nr. 78/2000, cu aplic. art. 5 din Codul penal, în infracţiunea de luare de mită în formă continuată, prev. de art. 6 din Legea nr. 78/2000 rap. la art. 289 al. 1 din Codul penal, rap. la art. 7 alin.1 lit. b din Legea nr. 78/2000, cu aplic. art. 35 al. 1 din Codul penal şi art. 5 din Codul penal (5 acte materiale), cu privire la inculpatul POPA VASILE CONSTANTIN; 
+- s-a dispus schimbarea încadrării juridice a infracţiunii de trafic de influenţă prev. de art. 6 din Legea nr. 78/2000  rap. la art. 291 din Codul penal., rap. la art. 7 alin.1, lit. b din Legea nr. 78/2000, cu aplic. art. 5 din Codul penal, în infracţiunea de trafic de influenţă prev. de art. 6 din Legea nr. 78/2000  rap. la art. 291 al. 1 din Codul penal, rap. la art. 7 alin.1, lit. b din Legea nr. 78/2000, cu aplic. art. 5 din Codul penal, cu privire la inculpatul POPA VASILE CONSTANTIN; 
+- s-a dispus schimbarea încadrării juridice a infracţiunii de sustragere sau distrugere de înscrisuri, prev. de art. 242 alin. 1 şi 3 din Codul penal, cu aplic. art. 5 din Codul penal, în infracţiunea de sustragere sau distrugere de înscrisuri prev. de art. 259 al. 1 şi 2 din Codul penal, cu aplic. art. 5 din Codul penal (3 infracţiuni), cu privire la inculpatul POPA VASILE CONSTANTIN; 
+- s-a dispus schimbarea încadrării juridice a infracţiunii de instigare la efectuarea unei prelevări atunci când prin aceasta se compromite o autopsie medico-legală, prev. de art. 47 din Codul penal rap. la art. 156 din Legea nr. 95/2006 republicată, în infracţiunea de instigare la efectuarea unei prelevări atunci când prin aceasta se compromite o autopsie medico-legală, prev. de art. 47 din Codul penal rap. la art. 155 din Legea nr. 95/2006 republicată, cu privire la inculpatul POPA VASILE CONSTANTIN; 
+- s-a dispus schimbarea încadrării juridice a infracţiunii de cumpărare de influenţă, prev. de art. 6 din Legea nr. 78/2000 rap. la art. 292 Codul penal, cu aplic. art. 5 din Codul penal, în infracţiunea de cumpărare de influenţă, prev. de art. 6 din Legea nr. 78/2000 rap. la art. 292 al. 1 din Codul penal, cu aplic. art. 5 din Codul penal, cu privire la inculpatul DAVID FLORIAN ALIN; 
+- s-a dispus schimbarea încadrării juridice a infracţiunii de efectuarea unei prelevări atunci când prin aceasta se compromite o autopsie medico-legală, prev. de art. 156 din Legea nr. 95/2006 republicată, în infracţiunea de efectuarea unei prelevări atunci când prin aceasta se compromite o autopsie medico-legală, prev. de art. 155 din Legea nr. 95/2006 republicată, cu privire la inculpatul MIHALACHE GABRIEL CONSTANTIN.
+	I. În baza art. 396 al. 1 şi 5 din Codul de procedură penală rap. la art. 16 al. 1 lit. b din Codul de procedură penală a fost achitat inculpatul POPA VASILE CONTASNTIN, sub aspectul săvârşirii infracţiunii de luare de mită în formă continuată, prev. de art. 6 din Legea nr. 78/2000 rap. la art. 289 al. 1 din Codul penal, rap. la art. 7 alin.1 lit. b din Legea nr. 78/2000, cu aplic. art. 35 al. 1 Codul penal şi art. 5 din Codul penal (5 acte materiale).
+În baza art. 396 al. 1 şi 5 din Codul de procedură penală rap. la art. 16 al. 1 lit. a din Codul de procedură penală a fost achitat inculpatul POPA VASILE CONSTANTIN, pentru săvârşirea infracţiunii de abuz în serviciu, prev. de art. 132 din Legea nr. 78/2000 modificată, cu referire la art. 297 alin. 1 din Codul 
+
+
+penal, cu aplic. art. 5 din Codul penal în legătură cu dosarul penal nr. 7157/P/2008 al Parchetului de pe lângă Judecătoria Oradea.
+"""
         
       }
   
