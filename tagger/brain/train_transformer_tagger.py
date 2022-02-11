ff3ce2be0dbdfe1f1a8f024dd7d22dcafc8cc77d
MihaiMasala <mihaimasala@gmail.com>
2022-02-11 21:59:39 +0200

    Added pre-computing step for tokenized inputs

--- tagger/brain/train_transformer_tagger.py
+++ tagger/brain/train_transformer_tagger.py
@@ -29,16 +29,15 @@ def build_model(bert_model, number_of_labels):
 
     input_ids      = layers.Input(shape=(MAX_LENGTH,), dtype='int64', name="input_ids")
     attention_mask = layers.Input(shape=(MAX_LENGTH,), dtype='int32', name="attention_mask")
-    token_type_ids = layers.Input(shape=(MAX_LENGTH,), dtype='int32', name="token_type_ids")
 
     bert_model.trainable = False
-    bert_layer = bert_model([input_ids, attention_mask, token_type_ids])[0]
+    bert_layer = bert_model([input_ids, attention_mask])[0]
     # get cls output
     bert_output = layers.Lambda(lambda seq: seq[:, 0, :])(bert_layer)
     # add dropout?
     classifier_layer = layers.Dense(number_of_labels, activation="sigmoid")(bert_output)
 
-    model = keras.Model(inputs=[input_ids, attention_mask, token_type_ids], outputs=classifier_layer)
+    model = keras.Model(inputs=[input_ids, attention_mask], outputs=classifier_layer)
     model.build(input_shape=[(None, MAX_LENGTH), (None, MAX_LENGTH), (None, MAX_LENGTH)])
 
     model.summary()
@@ -48,9 +47,8 @@ def build_model(bert_model, number_of_labels):
     return model
 
 
-def build_dataset(documents, labels, labels_dict, tokenizer):
+def build_dataset(inputs, labels, labels_dict, tokenizer):
     
-    print("START")
     # build appropiate labels
     processed_labels = []
     for label in labels:
@@ -58,23 +56,22 @@ def build_dataset(documents, labels, labels_dict, tokenizer):
         for l in label:
             onehot_labels[labels_dict[l]] = 1
         processed_labels.append(onehot_labels)
-    print("DONE LABELS")
-    inputs = tokenizer(documents, padding="max_length", truncation=True, max_length=MAX_LENGTH, is_split_into_words=True)
-    print("DONE TOKENIZER")
+
+    # inputs = tokenizer(documents, padding="max_length", truncation=True, max_length=MAX_LENGTH, is_split_into_words=True)
 
     def generator():
         for x in range(len(labels)):
-            yield (inputs["input_ids"][x], inputs["attention_mask"][x], inputs["token_type_ids"][x]), processed_labels[x]
+            yield (inputs[0][x], inputs[1][x]), processed_labels[x]
 
     if USE_GENERATOR == False:
-        tf_dataset = tf.data.Dataset.from_tensor_slices(((inputs["input_ids"], inputs["attention_mask"], inputs["token_type_ids"]), processed_labels))
+        tf_dataset = tf.data.Dataset.from_tensor_slices(((inputs[0], inputs[1]), processed_labels))
     else:
         # build generator
-        tf_dataset = tf.data.Dataset.from_generator(generator, output_types=((tf.int32, tf.int32, tf.int32), tf.int32),  output_shapes=(((MAX_LENGTH), (MAX_LENGTH), (MAX_LENGTH)), (len(labels_dict))))
+        tf_dataset = tf.data.Dataset.from_generator(generator, output_types=((tf.int32, tf.int32), tf.int32),  output_shapes=(((MAX_LENGTH), (MAX_LENGTH)), (len(labels_dict))))
     return tf_dataset
 
 
-def load_data_splits(documents, labels):
+def load_data_splits(inputs, labels):
 
     if os.path.exists(DATA_PATH+"_train_idx.pkl") and os.path.exists(DATA_PATH+"_dev_idx.pkl") and os.path.exists(DATA_PATH+"_test_idx.pkl"):
         train_indexes = pickle.load(open(DATA_PATH+"_train_idx.pkl", "rb"))
@@ -83,7 +80,7 @@ def load_data_splits(documents, labels):
     
     else:
         # build indices: 80% train, 10% dev, 10% test
-        indexes = set(range(len(documents)))
+        indexes = set(range(len(inputs[0])))
         train_indexes = set(random.sample(indexes, int(0.8*len(indexes))))
         remaining_indexes = indexes.difference(train_indexes)
         dev_indexes = set(random.sample(remaining_indexes, int(0.1*len(indexes))))
@@ -93,36 +90,48 @@ def load_data_splits(documents, labels):
         pickle.dump(dev_indexes, open(DATA_PATH+"_dev_idx.pkl", "wb"))
         pickle.dump(test_indexes, open(DATA_PATH+"_test_idx.pkl", "wb"))
 
-    if len(train_indexes.union(dev_indexes).union(test_indexes)) != len(documents) or len(train_indexes.intersection(dev_indexes).intersection(test_indexes)) != 0:
+    if len(train_indexes.union(dev_indexes).union(test_indexes)) != len(inputs[0]) or len(train_indexes.intersection(dev_indexes).intersection(test_indexes)) != 0:
         print("Problem with indexes")
         print(len(train_indexes), len(dev_indexes), len(test_indexes))
-        print("Union:", len(train_indexes.union(dev_indexes).union(test_indexes)), "should be:", len(documents))
+        print("Union:", len(train_indexes.union(dev_indexes).union(test_indexes)), "should be:", len(inputs[0]))
         print("Intersection:", len(train_indexes.intersection(dev_indexes).intersection(test_indexes)))
         sys.exit()
 
-    train_documents, train_labels, dev_documents, dev_labels, test_documents, test_labels = [], [], [], [], [], []
+    train_input_ids, train_input_attention, train_labels, dev_input_ids, dev_input_attention, dev_labels, test_input_ids, test_input_attention, test_labels = [], [], [], [], [], [], [], [], []
 
     for x in train_indexes:
-        train_documents.append(documents[x])
+        train_input_ids.append(inputs[0][x])
+        train_input_attention.append(inputs[1][x])
         train_labels.append(labels[x])
     
     for x in dev_indexes:
-        dev_documents.append(documents[x])
+        dev_input_ids.append(inputs[0][x])
+        dev_input_attention.append(inputs[1][x])
         dev_labels.append(labels[x])
     
     for x in test_indexes:
-        test_documents.append(documents[x])
+        test_input_ids.append(inputs[0][x])
+        test_input_attention.append(inputs[1][x])
         test_labels.append(labels[x])
     
-    return train_documents, train_labels, dev_documents, dev_labels, test_documents, test_labels
+    return [train_input_ids, train_input_attention], train_labels, [dev_input_ids, dev_input_attention], dev_labels, [test_input_ids, test_input_attention], test_labels
 
 
-def load_data():
-    documents = pickle.load(open(DATA_PATH + "_x_data.pkl", "rb"))
+def load_data(tokenizer):
+    # check if inputs are pre-processed (i.e. passed through tokenizer) for BERT-based model
+    if not os.path.exists(DATA_PATH + "_x_data_inputs_len{0}.pkl".format(MAX_LENGTH)):
+        print("Building inputs file. This may take a while.")
+        documents = pickle.load(open(DATA_PATH + "_x_data.pkl", "rb"))
+        inputs = tokenizer(documents, padding="max_length", truncation=True, max_length=MAX_LENGTH, is_split_into_words=True)
+        pickle.dump([inputs["input_ids"], inputs["attention_mask"]], open(DATA_PATH+"_x_data_inputs_len{0}.pkl".format(MAX_LENGTH), "wb"))
+    else:
+        inputs = pickle.load(open(DATA_PATH + "_x_data_inputs_len{0}.pkl".format(MAX_LENGTH), "rb"))
+        
+
     labels = pickle.load(open(DATA_PATH + "_y_data.pkl", "rb"))
     labels_dict = pickle.load(open(DATA_PATH + "_labels_dict.pkl", "rb"))
 
-    train_documents, train_labels, dev_documents, dev_labels, test_documents, test_labels = load_data_splits(documents, labels)
+    train_documents, train_labels, dev_documents, dev_labels, test_documents, test_labels = load_data_splits(inputs, labels)
 
     return train_documents, train_labels, dev_documents, dev_labels, test_documents, test_labels, labels_dict
 
@@ -130,19 +139,18 @@ def load_data():
 if __name__ == "__main__":
 
     st = time.time()
-    train_documents, train_labels, dev_documents, dev_labels, test_documents, test_labels, labels_dict = load_data()
-    
     bert_model = TFBertModel.from_pretrained("readerbench/jurBERT-base")
     tokenizer = AutoTokenizer.from_pretrained("readerbench/jurBERT-base")
 
+    train_inputs, train_labels, dev_inputs, dev_labels, test_inputs, test_labels, labels_dict = load_data(tokenizer)
+
+    train_dataset = build_dataset(train_inputs, train_labels, labels_dict, tokenizer).batch(BATCH_SIZE)
+    dev_dataset = build_dataset(dev_inputs, dev_labels, labels_dict, tokenizer).batch(BATCH_SIZE)
+    test_dataset = build_dataset(test_inputs, test_labels, labels_dict, tokenizer).batch(BATCH_SIZE)
+
     model = build_model(bert_model, len(labels_dict))
 
-    train_dataset = build_dataset(train_documents, train_labels, labels_dict, tokenizer).batch(BATCH_SIZE)
-    dev_dataset = build_dataset(dev_documents, dev_labels, labels_dict, tokenizer).batch(BATCH_SIZE)
-    test_dataset = build_dataset(test_documents, test_labels, labels_dict, tokenizer).batch(BATCH_SIZE)
-
     model.fit(train_dataset, epochs=EPOCHS, validation_data=dev_dataset)
-    # model.evaluate(train_dataset)
     model.evaluate(dev_dataset)
     model.evaluate(test_dataset)
 
