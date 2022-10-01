9eafbe93fb0c0fe38c03a8331863a23b97a7b162
LegeAI <94859277+LegeAI@users.noreply.github.com>
2022-10-02 00:15:54 +0300

    Added user input for running

--- script_train_tags_qa.py
+++ script_train_tags_qa.py
@@ -122,10 +122,46 @@ def get_data_path_qa(data_file):
     
     return data_file.split("tags_titles_v")[0]+"tags_titles_v"+str(int(data_file.split("tags_titles_v")[1].split("_x")[0]))
 
+def get_user_input():
+  
+  data_source = input('Enter the source of the data: from the live database (from_db), from a .csv file (path to the file) or from a data folder (like tags_vX for get_tags or tags_tiltes_vX for get qa) [default: from_db]: ')
+  if data_source == '':
+    data_source = "from_db"
+  if data_source != "from_db" and not(data_source.endswith(".csv")) and not os.path.isfile(os.path.join(args.base_folder, "_data/"+data_source+"_labels_dict.pkl")):
+    print("Invalid option for data_source")
+    input("Press ENTER to exit.")
+    sys.exit()
+  
+  if args.task == "get_tags":
+    epochs = 5
+    batch_size = 4
+    bert_max_seq_len = 512
+    
+  elif args.task == "get_qa":
+    epochs = 17
+    batch_size = 32
+    bert_max_seq_len = 64
+  
+  epochs_i = input('Enter the number of training epochs [{0}]: '.format(epochs))
+  if epochs_i != '':
+    epochs = int(epochs_i)
+    
+  batch_size_i = input('Enter the batch size [{0}]: '.format(batch_size))
+  if batch_size_i != '':
+    batch_size = int(batch_size_i)
+  
+  bert_max_seq_len_i = input('Enter the size of BERT sequnce length (max 512) [{0}]: '.format(bert_max_seq_len))
+  if bert_max_seq_len_i != '':
+    bert_max_seq_len = int(bert_max_seq_len_i)
+  
+  return data_source, epochs, batch_size, bert_max_seq_len
+  
+  
+    
 
 if __name__ == "__main__":
 
-    
+    data_source, epochs, batch_size, bert_max_seq_len = get_user_input()
     #region config
     if args.task == "get_tags":
         function_get_data = db_doc_text_saver.generate_data
@@ -133,9 +169,6 @@ if __name__ == "__main__":
         get_last_model_version = get_last_tags_model_version
         get_data_path = get_data_path_tags
         model_prefix = "tags"
-        epochs = 5
-        bert_max_seq_len = 512
-        batch_size = 4
 
     elif args.task == "get_qa":
         function_get_data = db_doc_title_saver.generate_data
@@ -143,19 +176,16 @@ if __name__ == "__main__":
         get_last_model_version = get_last_qa_model_version
         get_data_path = get_data_path_qa
         model_prefix = "tags_titles"
-        epochs = 17
-        bert_max_seq_len = 64
-        batch_size = 32
     #endregion config
     
     
     #region data
-    if args.data_source == "from_db" or args.data_source.endswith(".csv"):
+    if data_source == "from_db" or data_source.endswith(".csv"):
         # get data from db
-        data_file, labels_file, dict_file = function_get_data(debug=False, debug_save_count = 10000, source=args.data_source)
+        data_file, labels_file, dict_file = function_get_data(debug=False, debug_save_count = 5000, source=data_source)
         [data_file, labels_file, dict_file] = rename_files([data_file, labels_file, dict_file])
     else:
-        data_file = os.path.join(os.path.join(args.base_folder, "_data"), args.data_source)
+        data_file = os.path.join(os.path.join(args.base_folder, "_data"), data_source)
     #endregion data
 
     #region paths
@@ -170,7 +200,13 @@ if __name__ == "__main__":
     
     
     #region training
-    command = "python tagger\\brain\\train_transformer_tagger.py -use_generator -data_path={0} -k 1 3 5 -epochs={1} -learning_rate=1e-5 -bert_max_seq_len={2} -batch_size={3} -bert_backbone=readerbench/jurBERT-base -run_type=train_full -model_path={4} -dev_run=False".format(data_path, epochs, bert_max_seq_len, batch_size, model_path)
+    command = "python tagger\\brain\\train_transformer_tagger.py -use_generator -data_path={0} -k 3 -epochs={1} -learning_rate=1e-5 -bert_max_seq_len={2} -batch_size={3} -bert_backbone=readerbench/jurBERT-base -run_type=train_full -model_path={4} -dev_run=False".format(data_path, epochs, bert_max_seq_len, batch_size, model_path)
     subprocess.run(command)
     #endregion training
     
+    #region 
+    print("Used corpus:", data_path)
+    print("Trained model:", os.path.join(model_path, "weights/epoch{:02d}".format(epochs)))
+    #endregion
+    
+    input("Press ENTER to exit.")
